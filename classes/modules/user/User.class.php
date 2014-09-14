01ce03c89e6b0b3c57f14aaea1e33296971ab476
wasja1982 <wasja@yandex.ru>
2014-09-14 15:26:37 +0300

    Добавлен сброс ключа при выходе с сайта.

--- classes/modules/user/User.class.php
+++ classes/modules/user/User.class.php
@@ -10,15 +10,15 @@
  **/
 
 class PluginMultilogin_ModuleUser extends PluginMultilogin_Inherit_ModuleUser {
-	/**
-	 * Авторизовывает юзера
-	 *
-	 * @param ModuleUser_EntityUser $oUser	Объект пользователя
-	 * @param bool $bRemember	Запоминать пользователя или нет
-	 * @param string $sKey	Ключ авторизации для куков
-	 * @return bool
-	 */
-	public function Authorization(ModuleUser_EntityUser $oUser,$bRemember=true,$sKey=null) {
+    /**
+     * Авторизовывает юзера
+     *
+     * @param ModuleUser_EntityUser $oUser	Объект пользователя
+     * @param bool $bRemember	Запоминать пользователя или нет
+     * @param string $sKey	Ключ авторизации для куков
+     * @return bool
+     */
+    public function Authorization(ModuleUser_EntityUser $oUser,$bRemember=true,$sKey=null) {
         if ($oUser->isAdministrator()) {
             return parent::Authorization($oUser, $bRemember, $sKey);
         }
@@ -39,6 +39,24 @@ class PluginMultilogin_ModuleUser extends PluginMultilogin_Inherit_ModuleUser {
             setcookie('key',$sKey,time()+Config::Get('sys.cookie.time'),Config::Get('sys.cookie.path'),Config::Get('sys.cookie.host'),false,true);
         }
         return $bResult;
-	}
+    }
+    /**
+     * Разлогинивание
+     *
+     */
+    public function Logout() {
+        if ($this->oSession && $this->oUserCurrent && 
+            isset($_COOKIE['key']) && is_string($_COOKIE['key']) && $_COOKIE['key'] === $this->oSession->getKey()) {
+            $sKey = md5(func_generator() . time() . $this->oUserCurrent->getLogin());
+            $this->oSession->setKey($sKey);
+            $this->oMapper->CreateSession($this->oSession);
+            $data = array(
+                'time' => time(),
+                'session' => $this->oSession
+            );
+            $this->Cache_Set($data, "user_session_{$this->oSession->getUserId()}", array(), 60*60*24*4);
+        }
+        parent::Logout();
+    }
 }
 ?>
